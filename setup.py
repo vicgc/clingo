bcdc7a6f124c87bb5fd04d0142fcbf24e148b899
Khirod Kant Naik <khirod234@gmail.com>
2014-06-19 19:38:38 +0530

    packaged the module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+		name = "clingo",
+		packages = find_packages(),
+		version = "0.1",
+		description = "just an experimental tool",
+		author = "Khirod Kant Naik",
+		url = "https://github.com/shinigamiryuk/clingo",
+		scripts = ["scripts/clingo", "scripts/runFS"],
+)
